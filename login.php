0d4b6e5b97ec0b8cca0878602d3b8df5cfa9d5d5
bridgein <bridge.in.global@gmail.com>
2023-07-22 10:01:11 +0900

    initialize login.php

--- login.php
+++ login.php
@@ -1,9 +1,3 @@
 <?php
     echo "Hello, World!";
-    echo "Add user information input screen for login feature"
-    echo "Add authentication functionality to the login feature"
-    echo "Add log output to the login feature"
-    echo "Change file names related to the login feature"
-    echo "Add comments to the login feature"
-    echo "Add login button"
 ?>
